a2173f2992347bb6da4b9b891efab3bb9b5c3f30
AldricVS <aldric.silvestre@outlook.fr>
2021-01-08 11:01:53 +0100

    Debut class construction thompson et ajout d'images de doc

---
+++ src/process/factory/ThompsonAutomatonFactory.java
@@ -0,0 +1,15 @@
+package process.factory;
+
+import data.Automaton;
+
+/**
+ * Factory class that permits to create multiple Thompson's automatons from
+ * already existing automatons
+ * 
+ * @author Aldric Vitali Silvestre <aldric.vitali@outlook.fr>
+ */
+public class ThompsonAutomatonFactory {
+	
+	
+	
+}
