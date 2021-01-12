07a0b9d65a9b87f28dbda1ab7d3405d86c07daeb
AldricVS <aldric.silvestre@outlook.fr>
2021-01-13 00:37:03 +0100

    Erreur normalement corrigée, à voir si l'image est d'accord avec ça...

--- src/process/builders/ThompsonAutomatonBuilder.java
+++ src/process/builders/ThompsonAutomatonBuilder.java
@@ -59,30 +59,45 @@ public class ThompsonAutomatonBuilder {
 	 * @return the automaton that corresponds to the expression.
 	 * @throws ParseException if any problem during parsing the expression occurs
 	 */
-	public Automaton build() throws ParseException{
+	public Automaton build() throws ParseException {
 		Stack<Automaton> automatonStack = new Stack<>();
-		Stack<Character> operatorStack = new Stack<>(); 
+		Stack<Character> operatorStack = new Stack<>();
+
+		// ParenthesisWheight will permit to keep trace on all operation that we have to
+		// do and to avoid to miss any concatenation
+		int parentheisWeight = 0;
 
 		int i;
 		for (i = 0; i < expression.length(); i++) {
 			char currentChar = expression.charAt(i);
-			
-			if(currentChar == ' ' || currentChar == '(') {
+
+			if (currentChar == ' ') {
+				continue;
+			}else if(currentChar == '(') {
+				parentheisWeight++;
 				continue;
 			}
-			
-			if(isOperator(currentChar)) {
+
+			if (isOperator(currentChar)) {
 				operatorStack.push(currentChar);
-			}else if(currentChar == ')') {
+			} else if (currentChar == ')') {
+				parentheisWeight--;
 				// Get the needed operator and work depending on it
 				char operator;
 				try {
-					operator = operatorStack.pop();
-				}catch (EmptyStackException e) {
+					//don't pop if parenthesis weight too high
+					if(operatorStack.peek() == '*' || operatorStack.size() > parentheisWeight) {
+						operator = operatorStack.pop();
+					}else {
+						// we have others concatenations to do before
+						operator = '.';
+					}
+					
+				} catch (EmptyStackException e) {
 					// By default, we want to make a concatenation
 					operator = Character.MIN_VALUE;
 				}
-				
+
 				try {
 					switch (operator) {
 					case '*':
@@ -92,7 +107,8 @@ public class ThompsonAutomatonBuilder {
 						automatonStack.push(starAutomaton);
 						break;
 					case '+':
-						//we have the second automaton before the first one because of the stack insertion order
+						// we have the second automaton before the first one because of the stack
+						// insertion order
 						Automaton unionRight = automatonStack.pop();
 						Automaton unionLeft = automatonStack.pop();
 						Automaton unionAutomaton = ThompsonAutomatonFactory.createUnionAutomaton(unionLeft, unionRight);
@@ -103,31 +119,33 @@ public class ThompsonAutomatonBuilder {
 						// We have either operator '.' or not any operator, we will do a concatenation
 						Automaton concatRight = automatonStack.pop();
 						Automaton concatLeft = automatonStack.pop();
-						Automaton concatAutomaton = ThompsonAutomatonFactory.createConcatenationAutomaton(concatLeft, concatRight);
+						Automaton concatAutomaton = ThompsonAutomatonFactory.createConcatenationAutomaton(concatLeft,
+								concatRight);
 						automatonStack.push(concatAutomaton);
 						break;
 					default:
-						throw new ParseException("Unrecognized operator in stack", i);	
+						throw new ParseException("Unrecognized operator in stack", i);
 					}
-				}catch (EmptyStackException e) {
+				} catch (EmptyStackException e) {
 					// Here, we don't have enough automatons to use with the operator
 					throw new ParseException("Could not get enough automatons to work with operator " + operator, i);
 				}
-			}else {
-				//we have a normal character, create the letter automaton and add it to the stack
+			} else {
+				// we have a normal character, create the letter automaton and add it to the
+				// stack
 				Automaton letterAutomaton = ThompsonAutomatonFactory.createLetterAutomaton(currentChar);
 				automatonStack.push(letterAutomaton);
 			}
 		}
-		
+
 		// Normally, we have only one automaton remaining in the stack
-		if(automatonStack.size() != 1) {
+		if (automatonStack.size() != 1) {
 			throw new ParseException("Too many automatons left in the stack, operation probably not finished.", i);
-		}else {
+		} else {
 			return automatonStack.pop();
 		}
 	}
-	
+
 	private boolean isOperator(char character) {
 		return character == '+' || character == '.' || character == '*';
 	}
