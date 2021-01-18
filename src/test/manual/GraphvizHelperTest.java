8e8bdea2d5c02246b12fa226077929e5135e9ef6
AldricVS <aldric.silvestre@outlook.fr>
2021-01-18 11:56:52 +0100

    montrer plus d'exemples

--- src/test/manual/GraphvizHelperTest.java
+++ src/test/manual/GraphvizHelperTest.java
@@ -8,9 +8,13 @@ public class GraphvizHelperTest {
 
 	public static void main(String[] args) {
 		try {
+//			String fileInputPath = "\"C:/Program Files (x86)/Graphviz/tmp/test.dot\"";
+//			String fileOutputPath = "C:\\Creatomaton\\output\\billy.jpg";
+//			GraphvizHelper graphvizHelper = new GraphvizHelper(fileInputPath, fileOutputPath);
+//			graphvizHelper.runCommand();
+//			
 			String fileInputPath = "\"C:/Program Files (x86)/Graphviz/tmp/test.dot\"";
-			String fileOutputPath = "C:\\Creatomaton\\output";
-			GraphvizHelper graphvizHelper = new GraphvizHelper(fileInputPath, fileOutputPath);
+			GraphvizHelper graphvizHelper = new GraphvizHelper(fileInputPath);
 			graphvizHelper.setFileOutputName("billy.jpg");
 			graphvizHelper.runCommand();
 		} catch (IOException e) {
