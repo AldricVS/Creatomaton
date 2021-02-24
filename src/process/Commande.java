9fa01b66121b593184d899ae8652a8f028919527
chloe <chloe@LAPTOP-6SV2HCHP>
2021-02-24 22:18:06 +0100

    debut traitement line de commande

---
+++ src/process/Commande.java
@@ -0,0 +1,134 @@
+package process;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+
+import data.Automaton;
+import process.builders.AutomatonBuilder;
+import process.builders.ThompsonAutomatonBuilder;
+import process.factory.ThompsonAutomatonFactory;
+
+public class Commande {
+
+	private Options options = new Options();
+	
+	
+	public Commande () {
+		Options options = new Options();
+		
+		Option load = new Option("L","load",true,"charger un fichier .crea");
+		// dire leq fichier qu'il peu prendre .crea
+		load.setType(AutomatonManager.class);
+		Option random = new Option("R","random",true,"creer un automate aléatoire");
+		Option thompson = new Option("T","thompson",true,"creer un automate selon une expression");
+
+		// Creation du groupe d'option
+		OptionGroup group = new OptionGroup();
+		// Ajout des options exclusives
+		group.addOption(load) ;
+		group.addOption(random);
+		group.addOption(thompson);
+
+		// Possibilite de rendre un groupe obligatoire
+		group.setRequired(true);
+
+		// Ajout du groupe dans le conteneur Options 
+		options.addOptionGroup(group);
+		
+		Option sync = new Option("S","sync",false,"créer un automate synchronisé");
+		options.addOption(sync);
+		
+		Option det = new Option("D","det",false,"créer un automate déterministe");
+		options.addOption(det);
+
+		Option mir = new Option("M","mir",false,"créer un miroir de l'automate");
+		options.addOption(mir);
+		
+		Option mini = new Option("m","mini",false,"créer un automate minimal");
+		options.addOption(mini);
+		
+		Option all = new Option("A","all",false,"sync det mir et mini à la fois");
+		options.addOption(all);
+
+		Option val = new Option("V","val",false,"vérifie si l'automate valide le mot");
+		options.addOption(val);
+		
+		Option equi = new Option("E","equi",false,"vérifie l'équivalence avec un autre automate stocké dans un fichier .crea");
+		options.addOption(equi);
+
+		}
+		
+	public void traitement (String[] argument) {		
+		
+		CommandLineParser parser = new PosixParser();
+		Automaton automaton = null;
+		try {
+            CommandLine cmd = parser.parse(options, argument, false);
+
+            if(cmd.hasOption("help")){
+                // Affiche l'aide
+            	HelpFormatter formatter = new HelpFormatter();
+            	formatter.printHelp("MonServeur", options);
+            	
+            }
+            if(cmd.hasOption("load")){
+                System.out.println("récupération d'un fichier");
+                String fichier =  cmd.getOptionValue("load");
+                
+                
+            }
+            else if (cmd.hasOption("random")) {
+                System.out.println("création d'un automate aléatoire ");
+               String expression =cmd.getOptionValue("random");
+               
+         
+
+            }
+            else if (cmd.hasOption("thompson")) {
+                System.out.println("création d'un automate de thomson ");
+                String expression =cmd.getOptionValue("thompson");
+
+
+            }
+            if  (cmd.hasOption("syn")) {
+            	AutomatonBuilder builder = new AutomatonBuilder(automaton);
+            	builder.buildSynchronizedAutomaton();
+            }
+            if (cmd.hasOption("det")) {
+            	AutomatonBuilder builder = new AutomatonBuilder(automaton);
+            	builder.buildDeterministicAutomaton();
+            }
+            if (cmd.hasOption("min")) {
+            	AutomatonBuilder builder = new AutomatonBuilder(automaton);
+            	builder.buildMinimalAutomaton();
+            }
+            if (cmd.hasOption("mir")) {
+            	AutomatonBuilder builder = new AutomatonBuilder(automaton);
+            	builder.buildMirrorAutomaton();
+            }
+            if (cmd.hasOption("all")) {
+            	AutomatonBuilder builder = new AutomatonBuilder(automaton);
+            	builder.buildSynchronizedAutomaton();
+            	builder.buildDeterministicAutomaton();
+            	builder.buildMinimalAutomaton();
+            	builder.buildMirrorAutomaton();
+            }
+
+        } catch (ParseException e) {
+            // Affichage de l'aide
+        	HelpFormatter formatter = new HelpFormatter();
+        	formatter.printHelp("MonServeur", options);
+        	System.err.println("Parsing failed : " +e.getMessage());
+        }
+	}
+	
+}
+
+
