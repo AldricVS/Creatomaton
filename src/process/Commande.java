4c2e05f368452287520239f0179cdcfc372ddcd2
chloe <chloe@LAPTOP-8HC7DNEP.home>
2021-03-18 10:58:44 +0100

    avancement des lignes de commande

--- src/process/Commande.java
+++ src/process/Commande.java
@@ -3,6 +3,8 @@ package process;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Properties;
 
 import org.apache.commons.cli.CommandLine;
@@ -28,215 +30,224 @@ import process.file.ImageCreator;
 public class Commande {
 
 	private Options options;
-	
-	
-	public Commande () {
+	private HashMap<String, Automaton> listAutomaton = new HashMap<>();
+
+	public Commande() {
 		options = new Options();
-		
-		Option load = new Option("L","load",true,"charger un fichier .crea");
+
+		Option load = new Option("L", "load", true, "charger un fichier .crea");
 		// dire leq fichier qu'il peu prendre .crea
 		load.setType(AutomatonManager.class);
-		Option random = new Option("R","random",true,"creer un automate aléatoire");
-		Option thompson = new Option("T","thompson",true,"creer un automate selon une expression");
+		Option random = new Option("R", "random", true, "creer un automate aléatoire");
+		Option thompson = new Option("T", "thompson", true, "creer un automate selon une expression");
 
 		// Creation du groupe d'option
 		OptionGroup group = new OptionGroup();
 		// Ajout des options exclusives
-		group.addOption(load) ;
+		group.addOption(load);
 		group.addOption(random);
 		group.addOption(thompson);
 
 		// Possibilite de rendre un groupe obligatoire
 		group.setRequired(true);
 
-		// Ajout du groupe dans le conteneur Options 
+		// Ajout du groupe dans le conteneur Options
 		options.addOptionGroup(group);
-		
-		Option sync = new Option("S","sync",false,"créer un automate synchronisé");
+
+		Option sync = new Option("S", "sync", false, "créer un automate synchronisé");
 		options.addOption(sync);
-		
-		Option det = new Option("D","det",false,"créer un automate déterministe");
+
+		Option det = new Option("D", "det", false, "créer un automate déterministe");
 		options.addOption(det);
 
-		Option mir = new Option("M","mir",false,"créer un miroir de l'automate");
+		Option mir = new Option("M", "mir", false, "créer un miroir de l'automate");
 		options.addOption(mir);
-		
-		Option mini = new Option("m","mini",false,"créer un automate minimal");
+
+		Option mini = new Option("m", "mini", false, "créer un automate minimal");
 		options.addOption(mini);
-		
-		Option all = new Option("A","all",false,"sync det mir et mini à la fois");
+
+		Option all = new Option("A", "all", false, "sync det mir et mini à la fois");
 		options.addOption(all);
 
-		Option val = new Option("V","val",false,"vérifie si l'automate valide le mot");
+		Option val = new Option("V", "val", false, "vérifie si l'automate valide le mot");
 		options.addOption(val);
-		
-		Option equi = new Option("E","equi",false,"vérifie l'équivalence avec un autre automate stocké dans un fichier .crea");
+
+		Option equi = new Option("E", "equi", false,"vérifie l'équivalence avec un autre automate stocké dans un fichier .crea");
 		options.addOption(equi);
-		
-		
-		Option graphviz = new Option("G","graphviz",true,"export en image avec en paramètre le nom donné");
-		Option file = new Option("F","file",true,"export en fichier avec le nom du fichier passé");
+
+		Option graphviz = new Option("G", "graphviz", true, "export en image avec en paramètre le nom donné");
+		Option file = new Option("F", "file", true, "export en fichier avec le nom du fichier passé");
 
 		// Creation du groupe d'option
 		OptionGroup group2 = new OptionGroup();
 		// Ajout des options exclusives
-		group2.addOption(graphviz) ;
+		group2.addOption(graphviz);
 		group2.addOption(file);
 		// Possibilite de rendre un groupe obligatoire
 		group.setRequired(true);
-		// Ajout du groupe dans le conteneur Options 
+		// Ajout du groupe dans le conteneur Options
 		options.addOptionGroup(group2);
-		
 
-		}
-		
-	public void traitement (String[] argument) {		
-		
+	}
+
+	public void traitement(String[] argument) {
+
 		CommandLineParser parser = new DefaultParser();
 		try {
-            CommandLine cmd = parser.parse(options, argument, false);
-            ArrayList<Automaton> listAutomaton=null;
-            
-            if(cmd.hasOption("help")){
-                // Affiche l'aide
-            	HelpFormatter formatter = new HelpFormatter();
-            	formatter.printHelp("MonServeur", options);
-            	
-            }
-    		Automaton automaton = traitementAutomaton(cmd);
-    		listAutomaton.add(automaton);
-    		
-    		if (automaton != null) {
-    			automaton =traitementAlgo(cmd, automaton);
-    			listAutomaton.add(automaton);
-    			extraction(cmd,listAutomaton);
-    		}
-    		else {
-    			System.out.println("il faut d'abord utiliser soit random soit load soit thompson");
-    		}
-            
-            
-        } catch (ParseException e) {
-            // Affichage de l'aide
-        	HelpFormatter formatter = new HelpFormatter();
-        	formatter.printHelp("Commandes utilisables", options);
-        	System.err.println("Parsing failed : " + e.getMessage());
-        }
+			CommandLine cmd = parser.parse(options, argument, false);
+
+			if (cmd.hasOption("help")) {
+				// Affiche l'aide
+				HelpFormatter formatter = new HelpFormatter();
+				formatter.printHelp("MonServeur", options);
+
+			}
+			Automaton automaton = traitementAutomaton(cmd);
+			listAutomaton.put("base", automaton);
+
+			if (automaton != null) {
+				traitementAlgo(cmd, automaton);
+				extraction(cmd);
+			} else {
+				System.out.println("il faut d'abord utiliser soit random soit load soit thompson");
+			}
+
+		} catch (ParseException e) {
+			// Affichage de l'aide
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("Commandes utilisables", options);
+			System.err.println("Parsing failed : " + e.getMessage());
+		}
 	}
-	
-public Automaton traitementAutomaton( CommandLine cmd) {
-	Automaton automaton = null;
-	 if(cmd.hasOption("L")){
-         System.out.println("récupération d'un fichier");
-         String fichier =  cmd.getOptionValue("load");
-        automaton = load(fichier);
-     }
-     else if (cmd.hasOption("random")) {
-         System.out.println("création d'un automate aléatoire ");
-        String expression =cmd.getOptionValue("random");
-        automaton = random(expression);
-     }
-     else if (cmd.hasOption("thompson")) {
-         System.out.println("création d'un automate de thomson ");
-         String expression =cmd.getOptionValue("thompson");
-         automaton= thompson (expression);
-     }
-	 return automaton;
-}
 
+	public Automaton traitementAutomaton(CommandLine cmd) {
+		Automaton automaton = null;
+		if (cmd.hasOption("L")) {
+			System.out.println("récupération d'un fichier");
+			String fichier = cmd.getOptionValue("load");
+			automaton = load(fichier);
+		} else if (cmd.hasOption("random")) {
+			System.out.println("création d'un automate aléatoire ");
+			String expression = cmd.getOptionValue("random");
+			automaton = random(expression);
+		} else if (cmd.hasOption("thompson")) {
+			System.out.println("création d'un automate de thomson ");
+			String expression = cmd.getOptionValue("thompson");
+			automaton = thompson(expression);
+		}
+		return automaton;
+	}
 
+	public void traitementAlgo(CommandLine cmd, Automaton automaton) {
+		Automaton newAutomaton = null;
+		if (cmd.hasOption("all")) {
+			newAutomaton = synchronisation(automaton);
+			listAutomaton.put("syn", newAutomaton);
 
-public Automaton traitementAlgo(CommandLine cmd, Automaton automaton) {
-	Automaton newAutomaton = null;
-	if  (cmd.hasOption("syn")) {
-    	newAutomaton=synchronisation(automaton);
-    }
-    if (cmd.hasOption("det")) {
-    	newAutomaton=determinisation (automaton);
-    }
-    if (cmd.hasOption("min")) {
-    	newAutomaton=minimisation(automaton);
-    }
-    if (cmd.hasOption("mir")) {
-    	newAutomaton=miroir(automaton);
-    }
-    if (cmd.hasOption("all")) {
-    	newAutomaton=synchronisation(automaton);
-    	newAutomaton=determinisation(automaton);
-    	newAutomaton=minimisation(automaton);
-    	newAutomaton=miroir(automaton);
-    }
-	return newAutomaton;
+			newAutomaton = determinisation(automaton);
+			listAutomaton.put("det", newAutomaton);
 
-}
+			newAutomaton = minimisation(automaton);
+			listAutomaton.put("min", newAutomaton);
 
-	public Automaton random(String expression ) {
+			newAutomaton = miroir(automaton);
+			listAutomaton.put("mir", newAutomaton);
+
+		} else {
+			if (cmd.hasOption("syn")) {
+
+				newAutomaton = synchronisation(automaton);
+				listAutomaton.put("syn", newAutomaton);
+			}
+			if (cmd.hasOption("det")) {
+				newAutomaton = determinisation(automaton);
+				listAutomaton.put("det", newAutomaton);
+
+			}
+			if (cmd.hasOption("min")) {
+				newAutomaton = minimisation(automaton);
+				listAutomaton.put("min", newAutomaton);
+
+			}
+			if (cmd.hasOption("mir")) {
+				newAutomaton = miroir(automaton);
+				listAutomaton.put("mir", newAutomaton);
+
+			}
+		}
+
+	}
+
+	public Automaton random(String expression) {
 		RandomAutomatonBuilder automatonRandom = new RandomAutomatonBuilder();
 		Automaton automaton = null;
 		try {
-				// appelle de la methode qui renvoie un automate d'après l'expression
+			// appelle de la methode qui renvoie un automate d'après l'expression
 		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
 		}
 		return automaton;
 	}
-	
+
 	public Automaton load(String fichier) {
-		 AutomatonFileHelper automatonFileHelper = new AutomatonFileHelper();
-		 Automaton automaton = null;
-         try {
-				automaton = automatonFileHelper.loadAutomaton(fichier);
-			} catch (IllegalArgumentException | FileFormatException | IOException e) {
-				e.printStackTrace();
-			}
-         return automaton;
-		
+		AutomatonFileHelper automatonFileHelper = new AutomatonFileHelper();
+		Automaton automaton = null;
+		try {
+			automaton = automatonFileHelper.loadAutomaton(fichier);
+		} catch (IllegalArgumentException | FileFormatException | IOException e) {
+			e.printStackTrace();
+		}
+		return automaton;
+
 	}
-	
-	public Automaton thompson (String expression) {
+
+	public Automaton thompson(String expression) {
 
 		ThompsonAutomatonFactory automatonThompson = new ThompsonAutomatonFactory();
 		Automaton automaton = null;
 		try {
-			
-				// appelle de la methode qui renvoie un automate  de thompson d'après l'expression
+
+			// appelle de la methode qui renvoie un automate de thompson d'après
+			// l'expression
 		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
 		}
 		return automaton;
-		
+
 	}
-	
-	public Automaton minimisation (Automaton automaton) {
+
+	public Automaton minimisation(Automaton automaton) {
 		AutomatonBuilder builder = new AutomatonBuilder(automaton);
 		builder.buildMinimalAutomaton();
 		return automaton;
 	}
-	
-	public Automaton determinisation (Automaton automaton) {
+
+	public Automaton determinisation(Automaton automaton) {
 		AutomatonBuilder builder = new AutomatonBuilder(automaton);
 		builder.buildDeterministicAutomaton();
 		return automaton;
 	}
-	
-	public Automaton miroir (Automaton automaton) {
+
+	public Automaton miroir(Automaton automaton) {
 		AutomatonBuilder builder = new AutomatonBuilder(automaton);
 		builder.buildMirrorAutomaton();
 		return automaton;
 	}
-	public Automaton synchronisation (Automaton automaton) {
+
+	public Automaton synchronisation(Automaton automaton) {
 		AutomatonBuilder builder = new AutomatonBuilder(automaton);
 		builder.buildSynchronizedAutomaton();
 		return automaton;
 	}
-	
-	public void extraction (CommandLine cmd , ArrayList<Automaton> listAutomaton) {
+
+	public void extraction(CommandLine cmd) {
 		if (cmd.hasOption("graphviz")) {
-	        String name =cmd.getOptionValue("graphviz");
-			for (Automaton automaton : listAutomaton) {
-				
-				try {
-					ImageCreator picture = new ImageCreator(automaton, name);
+			String name = cmd.getOptionValue("graphviz");
+            for(Entry<String, Automaton> entry : listAutomaton.entrySet()) {
+            	String key = entry.getKey(); 
+            	Automaton automaton = entry.getValue();
+        			try {
+					ImageCreator picture = new ImageCreator(automaton, name+"_"+key);
 					picture.createImageFile();
 				} catch (IllegalArgumentException e) {
 					// TODO Auto-generated catch block
@@ -245,25 +256,26 @@ public Automaton traitementAlgo(CommandLine cmd, Automaton automaton) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-		        // ajouter l'appel de la fonction qui permet de faire l'export avec graphviz pour automaton
-			}  
-	    }
-	    if (cmd.hasOption("file")) {
-	        String nameFile =cmd.getOptionValue("file");
-	        AutomatonFileHelper extractionFile =new AutomatonFileHelper();
-			for (Automaton automaton : listAutomaton) {
-					try {
-						extractionFile.saveAutomaton(automaton, nameFile);
-					} catch (IllegalArgumentException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+				// ajouter l'appel de la fonction qui permet de faire l'export avec graphviz
+				// pour automaton
 			}
-	    }
+		}
+		if (cmd.hasOption("file")) {
+			String nameFile = cmd.getOptionValue("file");
+			AutomatonFileHelper extractionFile = new AutomatonFileHelper();
+			 for(Entry<String, Automaton> entry : listAutomaton.entrySet()) {
+	            	String key = entry.getKey(); 
+	            	Automaton automaton = entry.getValue();
+				try {
+					extractionFile.saveAutomaton(automaton, nameFile+"_"+key);
+				} catch (IllegalArgumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
-
-
--- src/test/manual/CommandParseTest.java
+++ src/test/manual/CommandParseTest.java
@@ -5,7 +5,7 @@ import process.Commande;
 public class CommandParseTest {
 	
 	public static void main(String[] args) {
-		String arg[] = {"-L"};
+		String arg[] = {"-L","src/test/manual/creaFiles/automaton.crea","-all","-G","test"};
 		Commande commande = new Commande();
 		commande.traitement(arg);
 	}
