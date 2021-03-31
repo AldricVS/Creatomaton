4713b4e57ede9108ffce471ec31cf0a7cf37e383
Maxence <maxence.hennekein@orange.fr>
2021-03-31 19:51:34 +0200

    questionnaire que j'ai fait
    ptet qu'il sera correct apres la correction de la synchronisation

--- src/test/manual/ModularMoodleTest.java
+++ src/test/manual/ModularMoodleTest.java
@@ -7,9 +7,9 @@ public class ModularMoodleTest {
 
 	public static void main(String[] args) {
 		// Change only the first line in order to change the question type
-		QuestionBankGenerator questionBankGenerator = QuestionBankFactory.createIsDeterministicGenerator("Est déterministe ou pas", 60);
+		QuestionBankGenerator questionBankGenerator = QuestionBankFactory.createWriteSynchronisedStatesGenerator("MoodleTest", 10);
 		questionBankGenerator.setNumberOfStates(5);
-		questionBankGenerator.setAlphabet("abcd");
+		questionBankGenerator.setAlphabet("abc");
 		questionBankGenerator.setQuestionPoints(1.0);
 		questionBankGenerator.generateBankFile();
 	}
